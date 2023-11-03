5767f675cb51f0626ba53e9214e79bb8ce6d78df
Tesnim Mohammednur <147130462+tesnu@users.noreply.github.com>
2023-11-03 16:54:52 +0300

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://tesnu.github.io/bloodmanage/public/style.css" rel="stylesheet"/>
     <title>@yield('title')</title>
 </head>
 
