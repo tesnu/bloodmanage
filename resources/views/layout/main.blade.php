253fa296bce9dddfe442e87a1fb00d6eb717e582
Metmiku Yohannes Bizuneh <met123y@gmail.com>
2023-11-03 12:48:34 +0300

    setup tailwind

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('style.css')}}">
     <title>@yield('title')</title>
 </head>
 
