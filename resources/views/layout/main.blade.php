ba8253f5f631592ea23bfccd3abd7ea1d4d7bac4
Tesnim Mohammednur <147130462+tesnu@users.noreply.github.com>
2023-11-03 16:39:56 +0300

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('style.css')}}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+      screens: {
+        'xs': '480px',
+      },
+      spacing: {
+        '30': '7.5rem',
+        '84': '21rem',
+        '88': '22rem',
+        '92': '23rem',
+        '104': '26rem',
+        '108': '27rem',
+        '112': '28rem',
+        '116': '29rem',
+        '120': '30rem',
+        '124': '31rem',
+        '128': '32rem',
+        '132': '33rem',
+        '136': '34rem',
+        '140': '35rem',
+        '144': '36rem',
+        '148': '37rem',
+        '152': '38rem',
+        '156': '39rem',
+        '160': '40rem',
+        '164': '41rem',
+        '168': '42rem',
+        '172': '43rem',
+        '176': '44rem',
+        '180': '45rem',
+        '184': '46rem',
+        '188': '47rem',
+        '196': '48rem',
+        '200': '49rem',
+        '204': '50rem',
+        '208': '51rem',
+        '212': '52rem',
+        '216': '53rem',
+        '220': '54rem',
+        '224': '55rem',
+        '228': '56rem',
+        '232': '57rem',
+        '236': '58rem',
+        '240': '59rem',
+        '244': '60rem',
+        '248': '61rem',
+        '252': '62rem',
+        '256': '63rem',
+        '260': '64rem',
+      }
+    },
+      }
+    }
+  </script>
     <title>@yield('title')</title>
 </head>
 
